68bbf9f0d831c2f24cbe084095891f725d51ffd0
USER-2020 <hackingetico2021@gmail.com>
2023-09-13 03:26:39 -0500

    feat: :bug:

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,6 +1,7 @@
 import Header from '../header'
 import Footer from '../footer'
-import { HomeComponent } from '../Home/HomeComponent.tsx'
+import { HomeComponent } from '../Home/homeComponent.tsx'
+
 
 
 const Home = () => {
