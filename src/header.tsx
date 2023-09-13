5c9b1538b682410956eae15f9be2d59b1e458da7
USER-2020 <hackingetico2021@gmail.com>
2023-09-13 18:35:09 -0500

    fix: :bug:

--- src/header.tsx
+++ src/header.tsx
@@ -1,51 +1,50 @@
 import { useState } from 'react';
 import './header_style.css';
 import { Collapse, Nav, NavItem, NavLink, Navbar, NavbarBrand, NavbarToggler } from 'reactstrap';
-// import { Link } from 'react-router-dom'; // Importa Link de React Router
+import { Link } from 'react-router-dom'; // Importa Link de React Router
 
 const Header = () => {
     const [collapsed, setCollapsed] = useState(true);
 
     const toggleNavbar = () => setCollapsed(!collapsed);
-  
+
     return (
-      <div>
-        <Navbar color="black" dark>
-          <NavbarBrand href="/" className="me-auto navTitle">
-            Candy Land Insights
-          </NavbarBrand>
-          <NavbarToggler onClick={toggleNavbar} className="me-2" />
-          <Collapse isOpen={!collapsed} navbar>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="/">Inicio</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/infoSustancias">
-                  Información sustancias
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/eventos">
-                  Eventos
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/playlist">
-                  Playlist
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/multimedia">
-                  Multimedia
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+        <div>
+            <Navbar color="black" dark>
+                <NavbarBrand tag={Link} to="/" className="me-auto navTitle">
+                    Candy Land Insights
+                </NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="me-2" />
+                <Collapse isOpen={!collapsed} navbar>
+                    <Nav navbar>
+                        <NavItem>
+                            <NavLink tag={Link} to="/">Inicio</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/infoSustancias">
+                                Información sustancias
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/eventos">
+                                Eventos
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/playlist">
+                                Playlist
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/multimedia">
+                                Multimedia
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
     );
-  }
-  
+}
 
 export default Header;
